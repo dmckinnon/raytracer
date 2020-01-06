05f3e11d78bdf91d633c3aa1ad36d4a52be77acc
damckinnon <insaynity@gmail.com>
2020-01-05 21:45:19 -0800

    All the basic files, not necessarily compiling, but its there

---
+++ RayTracer/RayTracer.cpp
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include "opencv2/core.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+#include <omp.h>
+
+using namespace std;
+using namespace cv;
+
+/*
+	This is an exercise in ray tracing/ray marching/path tracing/whatever. 
+
+	The aim is to do high quality ray tracing, denoising, with extra effects
+	like caustics and god rays
+
+
+*/
+
+int main(int argc, char** argv)
+{
+
+
+	return 0;
+}
